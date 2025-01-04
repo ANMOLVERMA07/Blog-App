d63a1d69100c904cd9c4f8630a0a31ec7624c9d7
ANMOLVERMA07 <153612130+ANMOLVERMA07@users.noreply.github.com>
2025-01-04 06:13:59 +0000

    some changes

--- Blig/src/components/RTE.jsx
+++ Blig/src/components/RTE.jsx
@@ -1,6 +1,7 @@
 import React from 'react'
 import {Editor} from '@tinymce/tinymce-react'
 import {Controller} from 'react-hook-form'
+import conf from '../conf/conf'
 
 export default function RTE({name,control,label,defaultValue=''}) {
   return (
@@ -12,7 +13,7 @@ export default function RTE({name,control,label,defaultValue=''}) {
       control={control}
       render={({field : {onChange,value}}) => (
         <Editor 
-        apiKey='nbzvbqh9iia3knqfepmbbf190a71dxhhkn42spoogfvckdgt'
+        apiKey={conf.apiKey}
         initialValue={defaultValue}
         init={{
             initialValue: defaultValue,
--- Blig/src/conf/conf.js
+++ Blig/src/conf/conf.js
@@ -4,6 +4,7 @@ const conf = {
     databaseId : String(import.meta.env.VITE_APPWRITE_DATABASE_ID),
     collectionId : String(import.meta.env.VITE_APPWRITE_COLLECTION_ID),
     bucketId : String(import.meta.env.VITE_APPWRITE_BUCKET_ID),
+    apiKey : String(import.meta.env.VITE_RTE_API_KEY)
 }
 
 export default conf;
